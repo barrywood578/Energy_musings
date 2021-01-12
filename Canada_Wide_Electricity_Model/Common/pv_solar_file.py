b2be6b7dcffe3f9606f08d4a681b7bbb827ec3bd
Barry Wood <barrywood721@gmail.com>
2021-01-12 12:01:34 -0500

    MODEL: PV Solar generation file support
    PV solar generators capacity depends on time of day and location.
    Adding support for a solar generation file that can determine
    capacity based on UTC.
    Also added a unit test for the capcity file.

---
+++ Canada_Wide_Electricity_Model/Common/pv_solar_file.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+"""
+    Support for the common "pv solar generation" file format.
+
+    Each file line contains:
+    Universal Coordinated Time (UCT) Year, Month, Day, Hour
+    Local Year, Month, Day, Hour
+    Power in kilowatts, to one decimal place
+
+"""
+
+from optparse import OptionParser
+from collections import OrderedDict
+import operator
+import sys
+import os
+import logging
+import copy
+from datetime import datetime, timezone
+from common_defs import *
+
+class pv_solar_hour(object):
+    def __init__(self, values = ["0000", "00", "00", "24", "000000.0"]):
+        self.local_Y = ""
+        self.local_M = ""
+        self.local_D = ""
+        self.local_H = ""
+        self.capacity_kW = 0.0
+        self.set_pv_solar_hour_to_list(values)
+
+    def get_list_from_pv_solar_hour(self):
+        return [self.local_Y,
+                self.local_M,
+                self.local_D,
+                self.local_H,
+                self.capacity_kW]
+
+    def set_pv_solar_hour_to_list(self, the_list):
+        if not (len(the_list) == 5):
+            raise ValueError("List must have 5 entries, not '%s'" %
+                              ','.join(the_list))
+        self.local_Y   = str(the_list[0])
+        self.local_M   = str(the_list[1])
+        self.local_D   = str(the_list[2])
+        self.local_H   = str(the_list[3])
+        self.capacity_kW = float(the_list[4])
+
+class pv_solar_file(object):
+    pv_solar_file_header = "UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)"
+
+    def __init__(self, file_path = ""):
+        self.capacity = {}
+
+        if (file_path == ""):
+            return
+        self.read_pv_solar_file(file_path)
+
+    def read_pv_solar_file(self, path):
+        lines = []
+
+        with open(path, 'r') as the_file:
+            lines = [line.strip() for line in the_file.readlines()]
+
+        if (lines[0] != self.pv_solar_file_header):
+            raise ValueError("File header is '%s', not '%s'.  Halting." %
+                    (lines[0], self.pv_solar_file_header))
+
+        for line_num, line in enumerate(lines[1:]):
+            if ((line[0] != START_END) or (line[-1] != START_END)):
+                raise ValueError("File %s Line %d delimiters '%s' '%s' not '%s'"
+                                 "'%s'. Halting." %
+                                 (path, line_num+1, line[0], line[-1],
+                                                START_END, START_END))
+            toks = [tok.strip() for tok in line[1:-1].split(SEPARATOR)]
+            if len(toks) != 9:
+                raise ValueError("File %s Line %d Bad format '%s'" %
+                        (path, line_num+1, line))
+
+            try:
+                self.add_pv_solar_hour(path, line_num+1, toks[0], toks[1], toks[2], toks[3],
+                                                       toks[4], toks[5], toks[6], toks[7], toks[8])
+            except ValueError as e:
+                logging.warning("%s" % str(e))
+                raise ValueError("File %s Line %d Invalid format '%s'" %
+                        (path, line_num+1, line))
+
+    def validate_fields(self, path, line_num, UTC_Y, UTC_M, UTC_D, UTC_H,
+                            Local_Y, Local_M, Local_D, Local_H, Capacity):
+        try:
+            U_Y = str(int(UTC_Y))
+            U_M = str(int(UTC_M))
+            U_D = str(int(UTC_D))
+            U_H = str(int(UTC_H))
+            L_Y = str(int(Local_Y))
+            L_M = str(int(Local_M))
+            L_D = str(int(Local_D))
+            L_H = str(int(Local_H))
+            Cap = str(float(Capacity))
+            if (not ((int(U_M)  in range (1, 13)) and
+                     (int(L_M)  in range (1, 13)) and
+                     (int(U_D)  in range (1, 32)) and
+                     (int(L_D)  in range (1, 32)) and
+                     (int(U_H)  in range (0, 24)) and
+                     (int(L_H)  in range (0, 24)))):
+                raise ValueError("Dates out of range.")
+            if (float(Cap) < 0.0) or (float(Cap) > 1000000):
+                raise ValueError("Capacity is out of range.")
+        except ValueError as e:
+            logging.info(e)
+            raise ValueError("File %s Line %s Data validation error." %
+                             (path, str(line_num)))
+
+        return U_Y, U_M, U_D, U_H, L_Y, L_M, L_D, L_H, float(Cap)
+
+    def add_pv_solar_hour(self, path, line_num, UTC_Y, UTC_M, UTC_D, UTC_H, Local_Y, Local_M, Local_D, Local_H, Capacity):
+        (U_Y, U_M, U_D, U_H,
+         L_Y, L_M, L_D, L_H, cap) = self.validate_fields(path, line_num, UTC_Y, UTC_M, UTC_D, UTC_H,
+                                                     Local_Y, Local_M, Local_D, Local_H, Capacity)
+
+        if not U_Y in self.capacity.keys():
+            self.capacity[U_Y] = {}
+        if not U_M in self.capacity[U_Y].keys():
+            self.capacity[U_Y][U_M] = {}
+        if not U_D in self.capacity[U_Y][U_M].keys():
+            self.capacity[U_Y][U_M][U_D] = {}
+        if not U_H in self.capacity[U_Y][U_M][U_D].keys():
+            self.capacity[U_Y][U_M][U_D][U_H] = pv_solar_hour([L_Y, L_M, L_D, L_H, cap])
+            return
+        self.capacity[U_Y][U_M][U_D][U_H].capacity_kW += cap
+
+    def get_pv_solar_capacity(self, UTC):
+        try:
+            return self.capacity[str(UTC.year)][str(UTC.month)][str(UTC.day)][str(UTC.hour)].capacity_kW
+        except:
+            return LOAD_ERROR
+
+    def write_pv_solar_file(self):
+        if len(self.capacity) == 0:
+            print("PV Solar file is empty!")
+            return
+        print(self.pv_solar_file_header)
+        for year in self.capacity.keys():
+            for month in self.capacity[year].keys():
+                for day in self.capacity[year][month].keys():
+                    for hour in self.capacity[year][month][day].keys():
+                        field_vals = [year, month, day, hour]
+                        solar_hour = self.capacity[year][month][day][hour].get_list_from_pv_solar_hour()
+                        field_vals.extend([str(x) for x in solar_hour])
+                        line_text = SEPARATOR.join(field_vals)
+                        print("%s%s%s" % (START_END, line_text, START_END))
+
+def create_parser():
+    parser = OptionParser(description="PV Solar file support.")
+    parser.add_option('-d', '--pv_solar',
+            dest = 'pv_solar_file_paths',
+            action = 'store', type = 'string', default = "",
+            help = 'File path to pv_solar file.',
+            metavar = 'FILE')
+    return parser
+
+def main(argv = None):
+    logging.basicConfig(level=logging.WARN)
+
+    parser = create_parser()
+    if argv is None:
+        argv = sys.argv[1:]
+
+    (options, argv) = parser.parse_args(argv)
+    if len(argv) != 0:
+        print('Must enter at least one file path!')
+        print
+        parser.print_help()
+        return -1
+
+    pv_solar = pv_solar_file(options.pv_solar_file_paths)
+    pv_solar.write_pv_solar_file()
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ Canada_Wide_Electricity_Model/Common/pv_solar_file_unittest.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+"""
+    Unit test for "pv solar file" support.
+"""
+
+from pv_solar_file import pv_solar_hour, pv_solar_file
+from datetime import datetime
+
+import unittest
+import mock
+from unittest.mock import patch, mock_open, call
+import logging
+
+class TestPVSolarHour(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pv_sh = pv_solar_hour()
+        self.assertEqual(pv_sh.local_Y, "0000")
+        self.assertEqual(pv_sh.local_M, "00")
+        self.assertEqual(pv_sh.local_D, "00")
+        self.assertEqual(pv_sh.local_H, "24")
+        self.assertEqual(pv_sh.capacity_kW, 0.0)
+
+        pv_sh = pv_solar_hour(["2011", "10", "9", "8", "123456.7"])
+        self.assertEqual(pv_sh.local_Y, "2011")
+        self.assertEqual(pv_sh.local_M, "10")
+        self.assertEqual(pv_sh.local_D, "9")
+        self.assertEqual(pv_sh.local_H, "8")
+        self.assertEqual(pv_sh.capacity_kW, 123456.7)
+
+    def test_get_list_from_pv_solar(self):
+        pv_sh = pv_solar_hour(["2011", "10", "9", "8", "123456.7"])
+        self.assertEqual(pv_sh.local_Y, "2011")
+        self.assertEqual(pv_sh.local_M, "10")
+        self.assertEqual(pv_sh.local_D, "9")
+        self.assertEqual(pv_sh.local_H, "8")
+        self.assertEqual(pv_sh.capacity_kW, 123456.7)
+
+        items = pv_sh.get_list_from_pv_solar_hour()
+        self.assertEqual(items[0], "2011")
+        self.assertEqual(items[1], "10")
+        self.assertEqual(items[2], "9")
+        self.assertEqual(items[3], "8")
+        self.assertEqual(items[4], 123456.7)
+
+    def test_set_pv_solar_hour_to_list_failure(self):
+
+        with self.assertRaises(Exception) as context:
+            pv_sh = pv_solar_hour(["2011", "10", "9", "8", "123456.7", 'extra'])
+        #print("\nList 1 Context: '%s'" % str(context.exception))
+        self.assertTrue("List must have 5 entries, not '2011,10,9,8,123456.7,extra'"
+                        in str(context.exception))
+
+        with self.assertRaises(Exception) as context2:
+            pv_sh = pv_solar_hour(["2011", "10", "9", "8 and missing"])
+        #print("\nList 2 Context: '%s'" % str(context2.exception))
+        self.assertTrue(("List must have 5 entries, not '2011,10,9,8 and missing'")
+                         in str(context2.exception))
+
+
+class TestPVSolarFile(unittest.TestCase):
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
+
+    def tearDown(self):
+        logging.disable(logging.NOTSET)
+
+    def test_constants(self):
+        pv = pv_solar_file()
+        self.assertEqual(pv.pv_solar_file_header, ("UTC_Year, UTC_Month, UTC_Day, "
+                                                   "UTC_Hour, Year, Month, Day, Hour, Capacity(kW)"))
+
+    def test_init(self):
+        pv = pv_solar_file()
+        self.assertEqual(pv.capacity, {})
+
+    def test_read_pv_solar_file_success(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', '12300.0'\n"
+                    "'2000', '1', '4', '9', '2000', '3', '4', '9', '1230.0'\n"
+                    "'2000', '1', '4', '10', '2000', '4', '5', '10', '123.0'\n"
+                    "'2000', '2', '5', '11', '2000', '5', '6', '11', '12.3'\n"
+                    "'2000', '2', '5', '12', '2000', '6', '7', '12', '1.23'\n"
+                    "'2000', '2', '6', '13', '2000', '7', '8', '13', '345.0'\n"
+                    "'2000', '2', '6', '14', '2000', '8', '9', '14', '100000.0'")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            pv = pv_solar_file()
+            self.assertFalse(mock_file.called)
+
+            pv.read_pv_solar_file("TestFile")
+            mock_file.assert_called_with("TestFile", 'r')
+            self.assertEqual(len(pv.capacity.keys()), 1)
+
+            lines = [line.strip() for line in file_data.split("\n")]
+            lines = [[tok.strip() for tok in line[1:-1].split("', '")] for line in lines[1:]]
+            for line in lines:
+                self.assertTrue(line[0] in pv.capacity)
+                self.assertTrue(line[1] in pv.capacity[line[0]])
+                self.assertTrue(line[2] in pv.capacity[line[0]][line[1]])
+                self.assertTrue(line[3] in pv.capacity[line[0]][line[1]][line[2]])
+                self.assertEqual(pv.capacity[line[0]][line[1]][line[2]][line[3]].local_Y, line[4])
+                self.assertEqual(pv.capacity[line[0]][line[1]][line[2]][line[3]].local_M, line[5])
+                self.assertEqual(pv.capacity[line[0]][line[1]][line[2]][line[3]].local_D, line[6])
+                self.assertEqual(pv.capacity[line[0]][line[1]][line[2]][line[3]].local_H, line[7])
+                self.assertEqual(pv.capacity[line[0]][line[1]][line[2]][line[3]].capacity_kW, float(line[8]))
+    # Test bad header in file
+    def test_read_pv_solar_file_fail_1(self):
+        file_data= ("Bad Header\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', '12300.0'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            with self.assertRaises(Exception) as context:
+                pv = pv_solar_file("TestFile")
+                self.assertEqual(pv.capacity, {})
+
+            #print("\nHeader Context: '%s'" % str(context.exception))
+            self.assertTrue(("File header is 'Bad Header', not "
+                             "'UTC_Year, UTC_Month, UTC_Day, UTC_Hour, "
+                             "Year, Month, Day, Hour, Capacity(kW)'.  Halting.")
+                             in str(context.exception))
+
+    # Test bad delimiters
+    def test_read_pv_solar_file_fail_2(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "X2000', '1', '3', '8', '2000', '2', '3', '8Y\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            with self.assertRaises(Exception) as context:
+                pv = pv_solar_file("TestFile")
+                self.assertTrue("2000" in pv.capacity)
+                self.assertTrue("1" in pv.capacity["2000"])
+                self.assertTrue("3" in pv.capacity["2000"]["1"])
+                self.assertTrue("7" in pv.capacity["2000"]["1"]["3"])
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_Y, "2000")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_M, "1")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_D, "2")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_H, "7")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].capacity_kW, 123000.0)
+
+            # print("\nDelim Context: '%s'" % str(context.exception))
+            self.assertTrue("File TestFile Line 2 delimiters 'X' 'Y' not ''''''. Halting."
+                             in str(context.exception))
+
+    # Test bad date data
+    def test_read_pv_solar_file_fail_3(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'BADYEAR', '1', '3', '8', '2000', '2', '3', '8', '12300.0'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            with self.assertRaises(Exception) as context:
+                pv = pv_solar_file("TestFile")
+                self.assertTrue("2000" in pv.capacity)
+                self.assertTrue("1" in pv.capacity["2000"])
+                self.assertTrue("3" in pv.capacity["2000"]["1"])
+                self.assertTrue("7" in pv.capacity["2000"]["1"]["3"])
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_Y, "2000")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_M, "1")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_D, "2")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_H, "7")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].capacity_kW, 123000.0)
+
+            # print("\nFail 3 Context: '%s'" % str(context.exception))
+            self.assertTrue(("File TestFile Line 2 Invalid format "
+                             "''BADYEAR', '1', '3', '8', "
+                             "'2000', '2', '3', '8', '12300.0''")
+                             in str(context.exception))
+
+    # Test bad capacity data
+    def test_read_pv_solar_file_fail_4(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', 'BADCAP'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            with self.assertRaises(Exception) as context:
+                pv = pv_solar_file("TestFile")
+                self.assertTrue("2000" in pv.capacity)
+                self.assertTrue("1" in pv.capacity["2000"])
+                self.assertTrue("3" in pv.capacity["2000"]["1"])
+                self.assertTrue("7" in pv.capacity["2000"]["1"]["3"])
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_Y, "2000")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_M, "1")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_D, "2")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_H, "7")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].capacity_kW, 123000.0)
+
+            # print("Context: '%s'" % str(context.exception))
+            self.assertTrue(("File TestFile Line 2 Invalid format "
+                             "''2000', '1', '3', '8', "
+                             "'2000', '2', '3', '8', 'BADCAP'")
+                             in str(context.exception))
+
+    # Test wrong number of items in line
+    def test_read_pv_solar_file_fail_5(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', '23456.0', 'Extra'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            with self.assertRaises(Exception) as context:
+                pv = pv_solar_file("TestFile")
+                self.assertTrue("2000" in pv.capacity)
+                self.assertTrue("1" in pv.capacity["2000"])
+                self.assertTrue("3" in pv.capacity["2000"]["1"])
+                self.assertTrue("7" in pv.capacity["2000"]["1"]["3"])
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_Y, "2000")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_M, "1")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_D, "2")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].local_H, "7")
+                self.assertEqual(pv.capacity["2000"]["1"]["3"]["7"].capacity_kW, 123000.0)
+
+            #print("\nFail 5 Context: '%s'" % str(context.exception))
+            self.assertTrue(("File TestFile Line 2 Bad format "
+                             "''2000', '1', '3', '8', "
+                             "'2000', '2', '3', '8', '23456.0', 'Extra''")
+                             in str(context.exception))
+
+    def test_validate_fields_success(self):
+        data = [["2000", "1", "1", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "12", "31", "23", "2000", "12", "31", "23", "1000000.0"]]
+
+        pv = pv_solar_file()
+        for item in data:
+            U_Y, U_M, U_D, U_H, L_Y, L_M, L_D, L_H, Cap = pv.validate_fields("NoFile", 100,
+                item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8])
+            self.assertEqual(U_Y, item[0])
+            self.assertEqual(U_M, item[1])
+            self.assertEqual(U_D, item[2])
+            self.assertEqual(U_H, item[3])
+            self.assertEqual(L_Y, item[4])
+            self.assertEqual(L_M, item[5])
+            self.assertEqual(L_D, item[6])
+            self.assertEqual(L_H, item[7])
+            self.assertEqual(Cap, float(item[8]))
+
+    def test_validate_fields_failure_conversion(self):
+        data = [["BadYear", "1", "1", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "BadMonth", "1", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "BadDay", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "1", "BadHour", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "1", "0", "BadYear", "1", "1", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "BadMonth", "1", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "1", "BadDay", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "1", "1", "BadHour", "0.0"],
+                ["2000", "1", "1", "0", "2000", "1", "1", "0", "BadCap"]]
+
+        pv = pv_solar_file()
+        for item in data:
+            with self.assertRaises(Exception) as context:
+                U_Y, U_M, U_D, U_H, L_Y, L_M, L_D, L_H, Cap = pv.validate_fields("NoFile", 100,
+                    item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8])
+            # print("Context: '%s'" % str(context.exception))
+            self.assertTrue("File NoFile Line 100 Data validation error." in str(context.exception))
+
+    def test_validate_fields_failure_dates(self):
+        data = [["2000", "0", "1", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "0", "0", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "1", "-1", "2000", "1", "1", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "0", "1", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "1", "0", "0", "0.0"],
+                ["2000", "1", "1", "0", "2000", "1", "1", "-1", "0.0"],
+                ["2000", "13", "31", "23", "2000", "12", "31", "23", "1000000.0"],
+                ["2000", "12", "32", "23", "2000", "12", "31", "23", "1000000.0"],
+                ["2000", "12", "31", "24", "2000", "12", "31", "23", "1000000.0"],
+                ["2000", "12", "31", "23", "2000", "13", "31", "23", "1000000.0"],
+                ["2000", "12", "31", "23", "2000", "12", "32", "23", "1000000.0"],
+                ["2000", "12", "31", "23", "2000", "12", "31", "24", "1000000.0"]]
+        pv = pv_solar_file()
+        for item in data:
+            with self.assertRaises(Exception) as context:
+                U_Y, U_M, U_D, U_H, L_Y, L_M, L_D, L_H, Cap = pv.validate_fields("NoFile", 100,
+                    item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8])
+            # print("\nDate Context: '%s'" % str(context.exception))
+            self.assertTrue("File NoFile Line 100 Data validation error."
+                            in str(context.exception))
+
+    def test_validate_fields_failure_capacity(self):
+        data = [ ["2000", "1", "1", "0", "2000", "1", "1", "0", "-1.0"],
+                ["2000", "12", "31", "23", "2000", "12", "31", "23", "1000000.1"]]
+        pv = pv_solar_file()
+        for item in data:
+            with self.assertRaises(Exception) as context:
+                U_Y, U_M, U_D, U_H, L_Y, L_M, L_D, L_H, Cap = pv.validate_fields("NoFile", 100,
+                    item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8])
+            # print("\nCapacity Context: '%s'" % str(context.exception))
+            self.assertTrue("File NoFile Line 100 Data validation error."
+                             in str(context.exception))
+
+    def test_get_pv_solar_capacity(self):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', '12300.0'\n"
+                    "'2000', '1', '4', '9', '2000', '3', '4', '9', '1230.0'\n"
+                    "'2000', '1', '4', '10', '2000', '4', '5', '10', '123.0'\n"
+                    "'2000', '2', '5', '11', '2000', '5', '6', '11', '12.3'\n"
+                    "'2000', '2', '5', '12', '2000', '6', '7', '12', '1.23'\n"
+                    "'2000', '2', '6', '13', '2000', '7', '8', '13', '345.0'\n"
+                    "'2000', '2', '6', '14', '2000', '8', '9', '14', '100000.0'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            pv = pv_solar_file("TestFile")
+            self.assertTrue(mock_file.called)
+            mock_file.assert_called_with("TestFile", 'r')
+
+            UTC = datetime(2000, 1, 3, hour=7)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 123000.0)
+
+            UTC = datetime(2000, 1, 3, hour=8)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 12300.0)
+
+            UTC = datetime(2000, 1, 4, hour=9)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 1230.0)
+
+            UTC = datetime(2000, 1, 4, hour=10)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 123.0)
+
+            UTC = datetime(2000, 2, 5, hour=11)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 12.30)
+
+            UTC = datetime(2000, 2, 5, hour=12)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 1.230)
+
+            UTC = datetime(2000, 2, 6, hour=13)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 345.0)
+
+            UTC = datetime(2000, 2, 6, hour=14)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, 100000.0)
+
+            UTC = datetime(2000, 7,12, hour=2)
+            cap = pv.get_pv_solar_capacity(UTC)
+            self.assertEqual(cap, -1.0)
+
+    @patch('builtins.print')
+    def test_write_pv_solar_file(self, mock_print):
+        file_data= ("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)\n"
+                    "'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'\n"
+                    "'2000', '1', '3', '8', '2000', '2', '3', '8', '12300.0'\n"
+                    "'2000', '1', '4', '9', '2000', '3', '4', '9', '1230.0'\n"
+                    "'2000', '1', '4', '10', '2000', '4', '5', '10', '123.0'\n")
+        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+            pv = pv_solar_file("TestFile")
+            self.assertTrue(mock_file.called)
+            pv.write_pv_solar_file()
+        self.assertEqual(mock_print.call_count, 5)
+        calls = [call("UTC_Year, UTC_Month, UTC_Day, UTC_Hour, Year, Month, Day, Hour, Capacity(kW)"),
+                 call("'2000', '1', '3', '7', '2000', '1', '2', '7', '123000.0'"),
+                 call("'2000', '1', '3', '8', '2000', '2', '3', '8', '12300.0'"),
+                 call("'2000', '1', '4', '9', '2000', '3', '4', '9', '1230.0'"),
+                 call("'2000', '1', '4', '10', '2000', '4', '5', '10', '123.0'")]
+        mock_print.assert_has_calls(calls, any_order = False)
+
+if __name__ == '__main__':
+    unittest.main()
