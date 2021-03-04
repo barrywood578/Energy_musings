b5e7da68af22174b597c743edb56bf3527e51f92
Barry Wood <barrywood721@gmail.com>
2021-03-04 16:02:08 -0500

    MODEL: File name standardization
    Changed all province specific file names (i.e. gen_db_AB.txt)
    to generic file names (gen_db.txt).
    Updated all scripts accordingly.

--- Canada_Wide_Electricity_Model/run_gen_data_assembly.sh
+++ Canada_Wide_Electricity_Model/run_gen_data_assembly.sh
@@ -27,42 +27,42 @@ fi
 echo 'KEY_FILE is ' ${KEY_FILE}
 
 echo 'British Columbia Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_British_Columbia' -t 'America/Vancouver' -k ${KEY_FILE} -s 01_British_Columbia/gen_pv_BC.txt -g Common/gen_db_GHG.txt > 01_British_Columbia/gen_db_BC.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_British_Columbia' -t 'America/Vancouver' -k ${KEY_FILE} -s 01_British_Columbia/gen_pv.txt -g Common/gen_db_GHG.txt > 01_British_Columbia/gen_db.txt
 check_rc 'British Columbia'
 
 echo 'Alberta Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Alberta' -t 'America/Edmonton' -k ${KEY_FILE} -s 02_Alberta/gen_pv_AB.txt -g Common/gen_db_GHG.txt > 02_Alberta/gen_db_AB.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Alberta' -t 'America/Edmonton' -k ${KEY_FILE} -s 02_Alberta/gen_pv.txt -g Common/gen_db_GHG.txt > 02_Alberta/gen_db.txt
 check_rc 'Alberta'
 
 echo 'Saskatchewan Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Saskatchewan' -t 'America/Regina' -k ${KEY_FILE} -s 03_Saskatchewan/gen_pv_SK.txt -g Common/gen_db_GHG.txt > 03_Saskatchewan/gen_db_SK.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Saskatchewan' -t 'America/Regina' -k ${KEY_FILE} -s 03_Saskatchewan/gen_pv.txt -g Common/gen_db_GHG.txt > 03_Saskatchewan/gen_db.txt
 check_rc 'Saskatchewan'
 
 echo 'Manitoba Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Manitoba' -t 'America/Winnipeg' -k ${KEY_FILE} -s 04_Manitoba/gen_pv_MB.txt -g Common/gen_db_GHG.txt > 04_Manitoba/gen_db_MB.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Manitoba' -t 'America/Winnipeg' -k ${KEY_FILE} -s 04_Manitoba/gen_pv.txt -g Common/gen_db_GHG.txt > 04_Manitoba/gen_db.txt
 check_rc 'Manitoba'
 
 echo 'Ontario Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Ontario' -t 'America/Toronto' -k ${KEY_FILE} -s 05_Ontario/gen_pv_ON.txt -g Common/gen_db_GHG.txt > 05_Ontario/gen_db_ON.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Ontario' -t 'America/Toronto' -k ${KEY_FILE} -s 05_Ontario/gen_pv.txt -g Common/gen_db_GHG.txt > 05_Ontario/gen_db.txt
 check_rc 'Ontario'
 
 echo 'Quebec Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Quebec' -t 'America/Montreal' -k ${KEY_FILE} -s 06_Quebec/gen_pv_PQ.txt -g Common/gen_db_GHG.txt > 06_Quebec/gen_db_PQ.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Quebec' -t 'America/Montreal' -k ${KEY_FILE} -s 06_Quebec/gen_pv.txt -g Common/gen_db_GHG.txt > 06_Quebec/gen_db.txt
 check_rc 'Quebec'
 
 echo 'New Brunswick Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_New_Brunswick' -t 'America/Moncton' -k ${KEY_FILE} -s 07_New_Brunswick/gen_pv_NB.txt -g Common/gen_db_GHG.txt > 07_New_Brunswick/gen_db_NB.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_New_Brunswick' -t 'America/Moncton' -k ${KEY_FILE} -s 07_New_Brunswick/gen_pv.txt -g Common/gen_db_GHG.txt > 07_New_Brunswick/gen_db.txt
 check_rc 'New Brunswick'
 
 echo 'Nova Scotia Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Nova_Scotia' -t 'America/Halifax' -k ${KEY_FILE} -s 08_Nova_Scotia/gen_pv_NS.txt -g Common/gen_db_GHG.txt > 08_Nova_Scotia/gen_db_NS.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Nova_Scotia' -t 'America/Halifax' -k ${KEY_FILE} -s 08_Nova_Scotia/gen_pv.txt -g Common/gen_db_GHG.txt > 08_Nova_Scotia/gen_db.txt
 check_rc 'Nova Scotia'
 
 echo 'Prince Edward Island Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Prince_Edward_Island' -t 'America/Charlottetown' -k ${KEY_FILE} -s 09_Prince_Edward_Island/gen_pv_PEI.txt -g Common/gen_db_GHG.txt > 09_Prince_Edward_Island/gen_db_PEI.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Prince_Edward_Island' -t 'America/Charlottetown' -k ${KEY_FILE} -s 09_Prince_Edward_Island/gen_pv.txt -g Common/gen_db_GHG.txt > 09_Prince_Edward_Island/gen_db.txt
 check_rc 'Prince Edward Island'
 
 echo 'Newfoundland and Labrador Starting...'
-./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Newfoundland_and_Labrador' -t 'America/St_Johns' -k ${KEY_FILE} -s 10_Newfoundland_and_Labrador/gen_pv_NL.txt -g Common/gen_db_GHG.txt > 10_Newfoundland_and_Labrador/gen_db_NL.txt
+./Common/generator_data_gathering.py -u 'https://en.wikipedia.org/wiki/List_of_generating_stations_in_Newfoundland_and_Labrador' -t 'America/St_Johns' -k ${KEY_FILE} -s 10_Newfoundland_and_Labrador/gen_pv.txt -g Common/gen_db_GHG.txt > 10_Newfoundland_and_Labrador/gen_db.txt
 check_rc 'Newfoundland and Labrador'
 
--- Canada_Wide_Electricity_Model/run_grid.sh
+++ Canada_Wide_Electricity_Model/run_grid.sh
@@ -18,33 +18,33 @@ function check_rc(){
 }
 
 echo 'Starting British Columbia...'
-Common/grid.py -d 01_British_Columbia/load_db_BC.txt -g 01_British_Columbia/gen_db_BC.txt -p 01_British_Columbia/gen_pv_BC.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 01_British_Columbia/load_db.txt -g 01_British_Columbia/gen_db.txt -p 01_British_Columbia/gen_pv.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'British Columbia'
 
 echo 'Starting Alberta...'
-Common/grid.py -d 02_Alberta/load_db_AB.txt -g 02_Alberta/gen_db_AB.txt -p 02_Alberta/gen_pv_AB.txt -w 02_Alberta/gen_wind_AB_actual.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 02_Alberta/load_db.txt -g 02_Alberta/gen_db.txt -p 02_Alberta/gen_pv.txt -w 02_Alberta/gen_wind.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'Alberta'
 
 echo 'Starting Ontario...'
-Common/grid.py -d 05_Ontario/load_db_ON.txt -g 05_Ontario/gen_db_ON.txt -p 05_Ontario/gen_pv_ON.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 05_Ontario/load_db.txt -g 05_Ontario/gen_db.txt -p 05_Ontario/gen_pv.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'Ontario'
 
 echo 'Starting Quebec...'
-Common/grid.py -d 06_Quebec/load_db_PQ.txt -g 06_Quebec/gen_db_PQ.txt -s "2014-01-01 05:00" -e "2015-01-01 04:00"
+Common/grid.py -d 06_Quebec/load_db.txt -g 06_Quebec/gen_db.txt -s "2014-01-01 05:00" -e "2015-01-01 04:00"
 check_rc load_file_PQ.txt
 
 echo 'Starting New Brunswick...'
-Common/grid.py -d 07_New_Brunswick/load_db_NB.txt -g 07_New_Brunswick/gen_db_NB.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 07_New_Brunswick/load_db.txt -g 07_New_Brunswick/gen_db.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'New Brunswick'
 
 echo 'Starting Nova Scotia...'
-Common/grid.py -d 08_Nova_Scotia/load_db_NS.txt -g 08_Nova_Scotia/gen_db_NS.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 08_Nova_Scotia/load_db.txt -g 08_Nova_Scotia/gen_db.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'Nova Scotia'
 
 echo 'Starting Prince Edward Island...'
-Common/grid.py -d 09_Prince_Edward_Island/load_db_PEI.txt -g 09_Prince_Edward_Island/gen_db_PEI.txt -w 09_Prince_Edward_Island/gen_wind_PEI_actual.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 09_Prince_Edward_Island/load_db.txt -g 09_Prince_Edward_Island/gen_db.txt -w 09_Prince_Edward_Island/gen_wind.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'Prince Edward Island Wind'
 
 echo 'Starting Newfoundland and Labrador...'
-Common/grid.py -d 10_Newfoundland_and_Labrador/load_db_NL.txt -g 10_Newfoundland_and_Labrador/gen_db_NL.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
+Common/grid.py -d 10_Newfoundland_and_Labrador/load_db.txt -g 10_Newfoundland_and_Labrador/gen_db.txt -s "2019-01-01 00:00" -e "2019-12-31 23:00"
 check_rc 'Newfoundland and Labrador'
